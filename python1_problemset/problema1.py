07b490a7c9fe0a34ad1bd0a63d1428399ce5e391
Brian <brianccardoso@usp.br>
2023-08-31 11:44:53 -0300

    finalizado os exs

--- python1_problemset/problema1.py
+++ python1_problemset/problema1.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
-print("Meu nome é: Brian")
+#declaração das variáveis do problema
+
+
+import sys
+
+
+
+nome = sys.argv[1]
+cor_favorita = sys.argv[2]
+atv_fav = sys.argv[3]
+fav_animal = sys.argv[4]
+
+print(f"Meu nome é {nome}")
+print(f"Minha cor favorita é {cor_favorita}")
+print(f"Minha atividade favorita é {atv_fav}")
+print(f"Meu animal favorito é {fav_animal}")
+
+
+
 
